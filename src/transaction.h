d9cabae7d45430c01f7f3340609fe06721ab6bdf
Andrew Vy <andrew@andrewvy.com>
2017-09-02 23:09:53 -0700

    :books: Add documentation around Transactions

--- src/transaction.h
+++ src/transaction.h
@@ -6,6 +6,23 @@
 
 #define INPUT_TXIN_HEADER_SIZE (32 + 4)
 
+/*
+ * Transactions can contain multiple InputTXs and multiple OutputTXs.
+ *
+ * InputTXs are treated as sources of value, pooled together and then split apart into one or more OutputTXs
+ * InputTXs reference a previous transaction hash where the value is coming from an earlier OutputTX (solidified in the blockchain)
+ * InputTXs contain a signature of the InputTX header, as well as the public key that signed it.
+ *   - The public key is converted into an Address to verify that the prev OutputTX was sent to the pubkey's address.
+ *   - This "unlocks" the previous OutputTX to be used as this current InputTX.
+ *   - The public key is used to verify the signature. (meaning the header has not been tampered.)
+ *   - From this, we know that:
+ *     - The person that is making this transaction owns the value designated by the transaction the InputTX(s) refer to.
+ *     - The person confirms that this transaction should be taking place.
+ *
+ * Once an OutputTX is used as an InputTX, it is considered spent. (All value is used from a OutputTX when being used as input)
+ * - If you don't want to spend everything from an InputTX, you can create a new OutputTX to send back to yourself as leftover-change.
+ */
+
 struct InputTransaction {
   // --- Header
   uint8_t transaction[32]; // Previous tx hash/id
